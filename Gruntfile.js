bf9e16aa756644f5ce9f157aa27b02aaa0f1c2c7
Mirza Sisic <mirzasisic57@gmail.com>
2018-06-26 11:37:51 -0400

    Concat css in order

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,13 +7,22 @@ module.exports = function(grunt){
         dest: 'build/scripts.js'
       },
       css: {
-        src: ['css/*.css'],
+        src: ['css/reset.css', 'css/bootstrap.css', 'css/styles.css'],
         dest: 'build/styles.css'
+      }
+    },
+    sass: {
+      build: {
+        files: [{
+          src: 'css/sass/styles.scss',
+          dest: 'css/styles.css'
+        }]
       }
     }
   });
   //Load a plugin
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-sass');
 
   //Register tasks
   grunt.registerTask('concat-js', ['concat:js']);
