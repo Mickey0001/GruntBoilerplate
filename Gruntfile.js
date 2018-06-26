6fb2ccb0a1fecd490854ac5254a9be1244589620
Mirza Sisic <mirzasisic57@gmail.com>
2018-06-26 11:05:56 -0400

    Grunt Tasks Test

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,5 +2,18 @@ module.exports = function(grunt){
   //Configuration
   grunt.initConfig({
     //Pass in option for plugins and refrence files
-  })
+  });
+  //Load a plugin
+  grunt.loadNpmTasks('');
+
+  //Register tasks
+  grunt.registerTask('run', function(){
+    console.log('Running!');
+  });
+
+  grunt.registerTask('sleep', function(){
+    console.log('Sleeping...');
+  });
+
+  grunt.registerTask('all', ['run', 'sleep']);
 }
